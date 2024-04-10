6f952c04d43b2a5500f7870b4ed047a54cefb48d
renan <renan.nic28@gmail.com>
2024-04-10 14:32:51 -0300

    first commit 10/04/2024

---
+++ src/main/java/com/pbd/ms_imobilt/MsImobiltApplication.java
@@ -0,0 +1,13 @@
+package com.pbd.ms_imobilt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsImobiltApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsImobiltApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pbd/ms_imobilt/MsImobiltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbd.ms_imobilt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsImobiltApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
