f3e1ce4159e7cbd1dcfcd6f51682da8cbbac2002
renan <renan.nic28@gmail.com>
2024-04-22 09:15:45 -0300

    create function validateBlocks

--- src/main/java/com/pbd/ms_imobilt/controller/EnterpriseController.java
+++ src/main/java/com/pbd/ms_imobilt/controller/EnterpriseController.java
@@ -5,13 +5,17 @@ import com.pbd.ms_imobilt.dto.EnterpriseReqDto;
 import com.pbd.ms_imobilt.dto.EnterpriseRespDto;
 import com.pbd.ms_imobilt.model.Enterprise;
 import com.pbd.ms_imobilt.service.EnterpriseService;
+import com.pbd.ms_imobilt.service.ValidationService;
+
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +25,8 @@ import org.springframework.web.bind.annotation.*;
 public class EnterpriseController {
 
     private final EnterpriseService enterpriseService;
+
+    private final ValidationService validationService;
 
     @PostMapping
     public ResponseEntity<EnterpriseRespDto> postCreateEnterprise(@RequestHeader(name = "token") String tokenHeader, @RequestBody @Valid EnterpriseReqDto requestBody){
@@ -37,9 +43,11 @@ public class EnterpriseController {
     }
 
     @PostMapping("{enterprise_id}/blocks")
-    public void postCreateBlocks(
+    public ResponseEntity<List<String>> postCreateBlocks(
             @PathVariable(name = "enterprise_id") Integer enterpriseId ,
             @RequestBody List<BlockReqDto> listBlocks){
+
+                return ResponseEntity.status(HttpStatus.CREATED).body(validationService.validateBlocks(listBlocks));
 
     }
 
